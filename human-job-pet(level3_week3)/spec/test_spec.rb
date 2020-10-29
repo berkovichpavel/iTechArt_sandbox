bcff5066ab06d1d1a4e324d9420573ea1ba33661
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-10-29 10:38:43 +0300

    Solve problems with tests

--- human-job-pet(level3_week3)/spec/test_spec.rb
+++ human-job-pet(level3_week3)/spec/test_spec.rb
@@ -17,24 +17,42 @@ describe Human do
   end
 
   describe 'Human - Animal' do
-    it 'man feeds the animal' do
-      expect(pet).to receive(:eat) { 45 } # mock
-      expect(human.feed_pet).to eq('You feed pet')
+    context 'When we fids animal' do
+      before do
+        allow(pet).to receive(:eat) { 45 } # stub
+      end
+      it 'man feeds the animal' do
+        expect(human.feed_pet).to eq('You feed pet')
+      end
     end
-    it 'animal is full ' do
-      expect(pet).to receive(:eat) { 'I\'m not hungry' } # mock
-      expect(human.feed_pet).to eq('Pet is full')
+
+    context 'When animal is full' do
+      before do
+        expect(pet).to receive(:eat) { 'I\'m not hungry' } # mock
+      end
+      it 'animal is full ' do
+        expect(human.feed_pet).to eq('Pet is full')
+      end
     end
-    it 'the person does  haven\'t enough money' do
-      expect(human).to receive(:money) { 0 } # mock
-      expect(human.feed_pet).to eq('You haven\'t got enough money to feed your pet! Go to work!')
+
+    context 'When human have not enough money' do
+      before do
+        expect(human).to receive(:money) { 0 } # mock
+      end
+      it 'the person does  haven\'t enough money' do
+        expect(human.feed_pet).to eq('You haven\'t got enough money to feed your pet! Go to work!')
+      end
     end
   end
 
   describe 'Human - Job' do
-    it 'man feeds the animal' do
-      allow(job).to receive(:get_salary) { 0 } # stub
-      expect(human.work).to eq('You get salary')
+    context 'When human work' do
+      before do
+        allow(job).to receive(:get_salary) { 0 } # stub
+      end
+      it 'man feeds the animal' do
+        expect(human.work).to eq('You get salary')
+      end
     end
   end
 end
